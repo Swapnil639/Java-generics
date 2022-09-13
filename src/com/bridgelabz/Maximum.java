42f27a2f6f908fc8ae3169c02976d91e60e0fa62
Swapnil639 <swapnilpatil631997@gmail.com>
2022-09-13 09:16:51 +0530

    Added :Refactored all three method to one generic method

--- src/com/bridgelabz/Maximum.java
+++ src/com/bridgelabz/Maximum.java
@@ -1,30 +1,8 @@
 package com.bridgelabz;
 
 public class Maximum {
-    public static Integer testCase(Integer a, Integer b, Integer c) {
-        Integer max = a;
-        if (b.compareTo(max) > 0) {
-            max = b;
-        }
-        if (c.compareTo(max) > 0) {
-            max = c;
-        }
-        return max;
-    }
-
-    public static Float testCase(Float a, Float b, Float c) {
-        Float max = a;
-        if (b.compareTo(max) > 0) {
-            max = b;
-        }
-        if (c.compareTo(max) > 0) {
-            max = c;
-        }
-        return max;
-    }
-
-    public static String testCase(String a, String b, String c) {
-        String max = a;
+    public static <T extends Comparable> T testCase(T a, T b, T c) {
+        T max = a;
         if (b.compareTo(max) > 0) {
             max = b;
         }
@@ -35,10 +13,11 @@ public class Maximum {
     }
 
     public static void main(String[] args) {
-        System.out.println("Welcome Generics program to find maximum");
-        System.out.println("Maximum Of Three Integers is :" + testCase(2, 8, 9));
-        System.out.println("Maximum Of Three Floats is :" + testCase(2.3f, 8.6f, 9.6f));
-        System.out.println("Maximum Of Three Strings is :" + testCase("Apple", "Elephant", "Banana"));
+        System.out.println("Welcome to maximum number program !");
+        System.out.println("Maximum among three integer values is :" + testCase(9, 5, 10));
+        System.out.println("Maximum among three float values is :" + testCase(9.8f, 5.7f, 101.7f));
+        System.out.println("Maximum among three string values is :" + testCase("Swapnil", "Sahil", "Shubham"));
+
 
     }
 }
