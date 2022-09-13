ed14e46a7b570fab55fc5fa0991e303678c51b55
Swapnil639 <swapnilpatil631997@gmail.com>
2022-09-13 09:07:54 +0530

    Added :Find maximum String in 3 Strings

--- src/com/bridgelabz/Maximum.java
+++ src/com/bridgelabz/Maximum.java
@@ -23,10 +23,22 @@ public class Maximum {
         return max;
     }
 
+    public static String testCase(String a, String b, String c) {
+        String max = a;
+        if (b.compareTo(max) > 0) {
+            max = b;
+        }
+        if (c.compareTo(max) > 0) {
+            max = c;
+        }
+        return max;
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome Generics program to find maximum");
         System.out.println("Maximum Of Three Integers is :" + testCase(2, 8, 9));
         System.out.println("Maximum Of Three Floats is :" + testCase(2.3f, 8.6f, 9.6f));
+        System.out.println("Maximum Of Three Strings is :" + testCase("Apple", "Elephant", "Banana"));
 
     }
 }
