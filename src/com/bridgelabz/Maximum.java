7a0da0396f46244c04f18185cf69f4d07a3634a1
Swapnil639 <swapnilpatil631997@gmail.com>
2022-09-13 09:04:36 +0530

    Added : Finding 3 Floats in a maximum

--- src/com/bridgelabz/Maximum.java
+++ src/com/bridgelabz/Maximum.java
@@ -12,9 +12,21 @@ public class Maximum {
         return max;
     }
 
+    public static Float testCase(Float a, Float b, Float c) {
+        Float max = a;
+        if (b.compareTo(max) > 0) {
+            max = b;
+        }
+        if (c.compareTo(max) > 0) {
+            max = c;
+        }
+        return max;
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome Generics program to find maximum");
         System.out.println("Maximum Of Three Integers is :" + testCase(2, 8, 9));
+        System.out.println("Maximum Of Three Floats is :" + testCase(2.3f, 8.6f, 9.6f));
 
     }
 }
